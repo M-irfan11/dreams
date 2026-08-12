939e3fd49f981e4eaa4f3156c5681480d0277ba5
Akibul Islam <akib34108@gmail.com>
2026-08-12 09:28:10 +0600

    update

--- connection-example.php
+++ connection-example.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $base_url = "http://localhost/dreams/";
-    require_once  ($_SERVER['DOCUMENT_ROOT'] . "/dreams/crud/crud_class.php");
+    $base_url = "http://localhost/supper-shop/";
+    require_once  ($_SERVER['DOCUMENT_ROOT'] . "/supper-shop/crud/crud_class.php");
     $crud = new crud_class();
 ?>
